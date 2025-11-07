81e7b8decedb5a01c108fe06e79ce47298377b46
cariomadaardisto <cariomadaardisto@gmail.com>
2025-11-07 21:34:42 +0700

    up

---
+++ context_test.go
@@ -0,0 +1,79 @@
+package belajargolangcontext
+
+import (
+	"context"
+	"fmt"
+	"runtime"
+	"testing"
+)
+
+func TestContext(t *testing.T) {
+	bg := context.Background()
+	fmt.Println(bg)
+
+	todo := context.TODO()
+	fmt.Println(todo)
+}
+
+func TestContextWithValue(t *testing.T) {
+	contextA := context.Background()
+
+	contextB := context.WithValue(contextA, "b", "B")
+	contextC := context.WithValue(contextA, "c", "C")
+
+	contextD := context.WithValue(contextB, "d", "D")
+	contextE := context.WithValue(contextB, "e", "E")
+
+	contextF := context.WithValue(contextC, "f", "F")
+	contextG := context.WithValue(contextF, "g", "G")
+
+	fmt.Println(contextA)
+	fmt.Println(contextB)
+	fmt.Println(contextC)
+	fmt.Println(contextD)
+	fmt.Println(contextE)
+	fmt.Println(contextF)
+	fmt.Println(contextG)
+
+	fmt.Println(contextF.Value("f"))
+	fmt.Println(contextF.Value("c"))
+	fmt.Println(contextF.Value("b"))
+
+	fmt.Println(contextA.Value("b"))
+}
+
+func CreateCounter(ctx context.Context) chan int {
+	destination := make(chan int)
+
+	go func ()  {
+		defer close(destination)
+		counter := 1
+		for {
+			select{
+			case <- ctx.Done():
+				return 
+			default:
+			destination <- counter
+			counter++
+			}
+		}
+	}()
+	return destination
+}
+
+func TestContextWithCancel(t *testing.T) {
+	fmt.Println(runtime.NumGoroutine())
+	parent := context.Background()
+	ctx, cancel := context.WithCancel(parent)
+
+	destination := CreateCounter(ctx)
+	for n := range destination{
+		fmt.Println("Counter", n)
+		if n == 10{
+			break
+		}
+	}
+	cancel()
+
+	fmt.Println(runtime.NumGoroutine())
+}
